e00487eb3a3838393e0887a57d83b60c6d42060d
kozabrada123 <koza@o7.si>
2025-09-04 18:04:54 +0200

    feat: some new files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@Autonomous(name = "AutonTest")
+public class AutonTest extends LinearOpMode {
+	@Override
+	public void runOpMode() {
+		waitForStart();
+		if (opModeIsActive()) {
+			// Pre-run
+			while (opModeIsActive()) {
+				// OpMode loop
+			}
+		}
+	}
+}
